5da404a5d06bf4255793d2d9415418be3126d5be
김학철 <hck916@hanpass.com>
2021-03-02 14:54:17 +0900

    first commit

---
+++ SwiftUiStudy/CustomTabView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabView.swift
+//  SwiftUiStudy
+//
+//  Created by 김학철 on 2021/03/02.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabView()
+    }
+}

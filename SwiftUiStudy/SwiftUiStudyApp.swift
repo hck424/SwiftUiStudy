ee67ffa741fa55a21b23dd3bcc241f447d929898
김학철 <hck916@hanpass.com>
2021-03-02 13:38:56 +0900

    Initial Commit

---
+++ SwiftUiStudy/SwiftUiStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStudyApp.swift
+//  SwiftUiStudy
+//
+//  Created by 김학철 on 2021/03/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
